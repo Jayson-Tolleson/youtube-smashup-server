9a4285e300e16c05ba6089bc237f8ce22a5084ae
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2023-02-06 08:34:34 -0800

    Create webserver.py
    Change to your cert and key location.

---
+++ webserver.py
@@ -0,0 +1,15 @@
+import os, sys 
+from http.server import HTTPServer, CGIHTTPRequestHandler 
+
+
+import ssl 
+
+webdir = '/var/youtube-smashup-server/' 
+port = 8080 
+os.chdir(webdir) 
+srvaddr=('', port) 
+
+srvobj = HTTPServer(srvaddr, CGIHTTPRequestHandler) 
+srvobj.socket = ssl.wrap_socket (srvobj.socket, keyfile='/var/security/lftr.biz.key', certfile='/var/security/lftr.biz.crt', server_side=True) 
+CGIHTTPRequestHandler.have_fork=False # Force the use of a subprocess 
+srvobj.serve_forever()
